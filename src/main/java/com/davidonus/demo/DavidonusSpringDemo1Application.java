ddd6ef47203eef94cecc768bf48dbbd14b42655a
David Morris <dnmorris7@gmail.com>
2019-06-04 13:26:47 -0400

    Initial Commit; Mistakes for module1 corrected

---
+++ src/main/java/com/davidonus/demo/DavidonusSpringDemo1Application.java
@@ -0,0 +1,15 @@
+package com.davidonus.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavidonusSpringDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavidonusSpringDemo1Application.class, args);
+		
+		
+	}
+
+}
---
+++ src/main/java/com/davidonus/demo/HomeController.java
@@ -0,0 +1,24 @@
+package com.davidonus.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@Controller
+public class HomeController {
+
+	
+	
+	
+	//return the home page
+	@RequestMapping("home")
+	public String home() {
+		System.out.println("Hi");
+
+		
+		return "home.jsp";
+	}
+}
+
